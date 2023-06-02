2cf32b472d4ed3de3357b891552a08b9df6089a4
saijeeth123 <132567865+saijeeth123@users.noreply.github.com>
2023-06-02 20:00:50 +0530

    Add files via upload

---
+++ Cucumber/Activities/Activityrunner.java
@@ -0,0 +1,16 @@
+package Activityrunner;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "Features",
+        glue = {"stepDefinitions"},
+        tags = "@activity2"
+)
+
+public class Activityrunner {
+    //empty
+}
